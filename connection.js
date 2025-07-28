bea7db0ae8e9876f4f431b3a583ad16bde6dc4c9
Kritika-17git <kritikadas1707@gmail.com>
2025-07-28 15:09:26 +0530

    connected to mongoDB atlas and updated env

--- connection.js
+++ connection.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 async function mongodbConnect(URI) {
-    return mongoose.connect(URI)
+    return mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }) 
     .then(()=>{
         console.log("Server connected");
     })
